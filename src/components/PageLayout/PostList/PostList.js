60c9839d804808db27efd966993de8d24223e22c
Andre Monello <atmonello@gmail.com>
2019-11-21 23:20:48 -0300

    Listando posts

--- src/components/PageLayout/PostList/PostList.js
+++ src/components/PageLayout/PostList/PostList.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './PostList.css'
 
 import Post from '../../Post/Post';
+import profilePicture from '../../../assets/profile.png'
 
 class PostList extends Component {
   state = {
     posts: [
       {
-        title: 'Lalalalalala',
+        postAuthor: 'André Monello',
+        postProfile: profilePicture,
+        date: '21 Nov 2019',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, animi esse officiis eaque quaerat veniam ipsam accusantium, assumenda, aliquid voluptatum exercitationem id! Quam sequi, consequuntur inventore suscipit dignissimos quae nemo!',
         comments: [
           {
             text: '1'
@@ -18,7 +22,10 @@ class PostList extends Component {
         ]
       },
       {
-        title: 'Lelelelele',
+        postAuthor: 'André Monello',
+        postProfile: profilePicture,
+        date: '21 Nov 2019',
+        text: 'Minima soluta autem necessitatibus, quis ratione, vitae officia hic earum molestiae rerum dolorum optio maiores libero amet iure cum porro perspiciatis aliquam animi aliquid. Consequatur repudiandae et sit obcaecati blanditiis.',
         comments: [
           {
             text: '1'
@@ -26,21 +33,10 @@ class PostList extends Component {
         ]
       },
       {
-        title: 'Lilililili',
-        comments: [
-          {
-            text: '1'
-          },
-          {
-            text: '2'
-          },
-          {
-            text: '3'
-          }
-        ]
-      },
-      {
-        title: 'Lololololo',
+        postAuthor: 'André Monello',
+        postProfile: profilePicture,
+        date: '21 Nov 2019',
+        text: 'Ipsum, sint. Voluptate pariatur aut quidem animi illo voluptatem ad repudiandae adipisci, similique hic eligendi amet aperiam ipsa maxime ipsum? Voluptatem enim recusandae ipsam facilis exercitationem repellendus. Inventore, enim alias.',
         comments: [
           {
             text: '1'
@@ -54,7 +50,27 @@ class PostList extends Component {
         ]
       },
       {
-        title: 'Lulululu',
+        postAuthor: 'André Monello',
+        postProfile: profilePicture,
+        date: '21 Nov 2019',
+        text: 'Officiis illo optio vel excepturi, in amet ipsam alias sed porro sit ut error sunt praesentium quia itaque deserunt quos a delectus, deleniti similique, nostrum aspernatur libero! Repudiandae, commodi distinctio?',
+        comments: [
+          {
+            text: '1'
+          },
+          {
+            text: '2'
+          },
+          {
+            text: '3'
+          }
+        ]
+      },
+      {
+        postAuthor: 'André Monello',
+        postProfile: profilePicture,
+        date: '21 Nov 2019',
+        text: 'Aspernatur iste totam ab perferendis delectus qui harum cum quidem blanditiis cupiditate explicabo, veritatis ea, accusantium incidunt vero, suscipit sint fuga praesentium dolor nostrum voluptas saepe nulla quis quam? Deleniti.',
         comments: [
           {
             text: '1'
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,12 +4,25 @@ import './Post.css'
 import Comment from './Comment/Comment';
 
 function Post({post}) {
-  const { title, comments } = post;
+  const { 
+    postAuthor, 
+    date, 
+    postProfile, 
+    text,
+    comments,
+  } = post;
 
   return (
     <li className="post-list-item">
       <div className="post-header">
-        <h3>{title}</h3>
+        <img className="post-profile" src={postProfile} />
+        <aside className="post-credits">
+          <p className="post-author">{postAuthor}</p>
+          <small className="post-date">{date}</small>
+        </aside>
+      </div>
+      <div className="post-content">
+        <p>{text}</p>
       </div>
       <div className="post-comments">
         {comments.map((comment, index) => <Comment key={index} content={comment.text} />)}
